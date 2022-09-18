b5eb151a0fdefc36408cee7cad18b2a3acc56c0e
JanKolo04 <jankolodziej04@gmail.com>
2022-09-18 21:34:08 +0200

    uzytkownicy file

---
+++ admin-panel/uzytkownicy.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/style-uzytkownicy.css">
+</head>
+<body>
+
+    <?php
+    
+        function show_all_users() {
+            global $con;
+            
+            
+        }
+
+    ?>
+
+</body>
+</html>
