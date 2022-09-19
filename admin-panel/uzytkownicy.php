5a8748caa3a6f20329de1e29ebd670c5183d71a5
JanKolo04 <jankolodziej04@gmail.com>
2022-09-19 21:50:46 +0200

    table with users

--- admin-panel/uzytkownicy.php
+++ admin-panel/uzytkownicy.php
@@ -12,10 +12,42 @@
         function show_all_users() {
             global $con;
             
-            
+            //find all users
+            $sql = "SELECT * FROM uzytkownicy";
+            //query
+            $query = mysqli_query($con, $sql);
+
+            if($query->num_rows > 0) {
+                while($row = mysqli_fetch_array($query)) {
+                    echo "
+                        <tr>
+                            <td>{$row['id_uzytkownik']}</td>
+                            <td>{$row['Imie']}</td>
+                            <td>{$row['Nazwisko']}</td>
+                            <td>{$row['Email']}</td>
+                        </tr>
+                    ";
+                }
+            }
+            else {
+                echo "<tr><td colspan='4'>Nie ma użytkowników</td></tr>";
+            }
         }
 
     ?>
+    <div id="tableDiv">
+        <table id="table-user">
+            <thead>
+                <th>Id</th>
+                <th>Imie</th>
+                <th>Nazwisko</th>
+                <th>Email</th>
+            </thead>
+            <tbody>
+                <?php show_all_users(); ?>
+            </tbody>
+        </table>
+    </div>
 
 </body>
 </html>
