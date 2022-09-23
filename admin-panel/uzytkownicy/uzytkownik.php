4a0d012984fcaf0ee1770aab08bf4b3e13943bac
JanKolo04 <jankolodziej04@gmail.com>
2022-09-23 13:09:21 +0200

    Create add user theme. This theme I will useing to register page

--- admin-panel/uzytkownicy/uzytkownik.php
+++ admin-panel/uzytkownicy/uzytkownik.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/style-uzytkownik.css">
 </head>
 <body>
 
@@ -53,14 +54,27 @@
 
     ?>
 
-    <form method="POST">
-        <input type="text" name="name" placeholder="Imie..." required>
-        <input type="text" name="surname" placeholder="Nazwisko..." required>
-        <input type="email" name="email" placeholder="Email..." required>
-        <input type="text" name="birth_date" placeholder="YYYY-MM-DD" pattern="(?:19|20)(?:(?:[13579][26]|[02468][048])-(?:(?:0[1-9]|1[0-2])-(?:0[1-9]|1[0-9]|2[0-9])|(?:(?!02)(?:0[1-9]|1[0-2])-(?:30))|(?:(?:0[13578]|1[02])-31))|(?:[0-9]{2}-(?:0[1-9]|1[0-2])-(?:0[1-9]|1[0-9]|2[0-8])|(?:(?!02)(?:0[1-9]|1[0-2])-(?:29|30))|(?:(?:0[13578]|1[02])-31)))" required>
+    <div id="addUserHolder">
+        <div id='header-add-user'>
+            <h2>Dodaj uytkownika</h2>
+        </div>
+        <div id="form-holder">
+            <form method="POST" id="form-add-user">
+                <div id="inputs-holder">
+                    <input type="text" name="name" placeholder="Imie..." required>
+                    <input type="text" name="surname" placeholder="Nazwisko..." required>
+                    <input type="email" name="email" placeholder="Email..." required>
+                    <input type="text" name="birth_date" placeholder="yyyy-mm-dd..." pattern="(?:19|20)(?:(?:[13579][26]|[02468][048])-(?:(?:0[1-9]|1[0-2])-(?:0[1-9]|1[0-9]|2[0-9])|(?:(?!02)(?:0[1-9]|1[0-2])-(?:30))|(?:(?:0[13578]|1[02])-31))|(?:[0-9]{2}-(?:0[1-9]|1[0-2])-(?:0[1-9]|1[0-9]|2[0-8])|(?:(?!02)(?:0[1-9]|1[0-2])-(?:29|30))|(?:(?:0[13578]|1[02])-31)))" required>
+                </div>
 
-        <button type="submit" name="add_user">Dodaj</button>
-    </form>
+                <div id="errors-holder"></div>
+
+                <div id="submit-holder">
+                    <button type="submit" name="add_user">Dodaj</button>
+                </div>
+            </form>
+        </div>
+    </div>
 
 </body>
 </html>
