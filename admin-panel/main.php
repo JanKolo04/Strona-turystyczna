58fefde4da8564dc08e95a4e1766a2a1a95de356
JanKolo04 <jankolodziej04@gmail.com>
2022-09-30 12:59:29 +0200

    Get all moves between current date and minus six date

--- admin-panel/main.php
+++ admin-panel/main.php
@@ -11,7 +11,30 @@
     
         function get_all_data() {
             global $con;
+
+            //get current date
+            $mydate=getdate(date("U"));
+            //set sql format date
+            $current_date = "$mydate[year]-$mydate[mon]-$mydate[mday]";
+            //from date
+            $date_create = date_create($current_date);
+            //minus six days form current date
+            $from_date = date_sub($date_create,date_interval_create_from_date_string("6 days"))->format('Y-m-d');
+
+            //seatrch moves
+            $sql_all = "SELECT * FROM Move WHERE Date BETWEEN '$from_date' AND '$current_date'";
+            $query_all = mysqli_query($con, $sql_all);
+
+            //echo  data
+            while($row = mysqli_fetch_array($query_all)) {
+                echo "User: ".$row['id_user']."</br>";
+                echo "Date: ".$row['Date']."</br></br>";
+            }
+            
         }
+
+        get_all_data();
+
     
     ?>
 
