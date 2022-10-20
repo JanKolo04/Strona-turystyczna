d8dc7469283a92a60e3d56f787a7a69c0119d897
JanKolo04 <jankolodziej04@gmail.com>
2022-10-20 23:37:42 +0200

    Print all favorite routes or objects

--- konto/favorite.php
+++ konto/favorite.php
@@ -9,5 +9,64 @@
 
     <h3>Polubione wycieczki</h3>
 
+	<?php
+	
+		/* -----Sktórić funkcjer tylko do jednej która będzie pobierała dane, a druga która będzie je wypisywać----- */
+
+
+		function get_all_objects() {
+			global $con;
+			//user_id from SESSION
+			$id_user = 3;
+
+			//get all objects where id_uzytkownik equals $id_user
+			$sql = "SELECT obiekty.* FROM ulubione INNER JOIN obiekty ON obiekty.Id=ulubione.Id_obiektu WHERE ulubione.id_uzytkownika=$id_user;";
+			$query = mysqli_query($con, $sql);
+
+
+			echo "<strong>Obiekty: </strong>";
+			//insert data into array objects
+			$objects_array = [];
+			if($query->num_rows > 0 && $query !== false) {
+				while($row = mysqli_fetch_array($query)) {
+					echo $row['Nazwa'];
+				}
+			}
+			else {
+				echo "Brak zapisanych obiektów";
+			}
+
+		}
+
+
+		function get_all_routes() {
+			global $con;
+			//user_id from SESSION
+			$id_user = 3;
+
+			//get all routes where id_uzytkownik equals $id_user
+			$sql = "SELECT trasy.* FROM ulubione INNER JOIN trasy ON trasy.Id=ulubione.Id_trasy WHERE ulubione.id_uzytkownika=$id_user;";
+			$query = mysqli_query($con, $sql);
+
+
+			echo "</br><strong>Trasy: </strong>";
+			//insert data into array objects
+			$routes_array = [];
+			if($query->num_rows > 0 && $query !== false) {
+				while($row = mysqli_fetch_array($query)) {
+					echo $row['Nazwa'];
+				}
+			}
+			else {
+				echo "Brak zapisanych tras";
+			}
+
+		}
+
+		get_all_objects();
+		get_all_routes();
+	
+	?>
+
 </body>
 </html>
