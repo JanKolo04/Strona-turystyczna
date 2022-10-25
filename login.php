f15a1796c4a0e5b4319aa5723b595e79662287f7
JanKolo04 <jankolodziej04@gmail.com>
2022-10-25 15:06:43 +0200

    create class to login

---
+++ login.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Zaloguj się - Szlakiem Szczecina</title>
+</head>
+<body>
+
+    <form method="POST">
+        <label for="input:[type=text]">Login</label>
+        <input type="text" name="login"></br>
+
+        <label for="input:[type=password]">Password</label>
+        <input type="password" name="password"></br>
+        
+        <label for="input:[type=checkbox]">Cookie</label>
+        <input type="checkbox" name="cookie"></br>
+
+        <button type="submit" name="submit">Sign in</button>
+    </form>
+
+    <?php
+    
+        session_start();
+
+        if(isset($_COOKIE['autologin'])) {
+            $_SESSION['login'] = $_COOKIE['autologin'];
+        }
+
+        if(isset($_POST['cookie'])) {
+            //set COOKIE for autologin
+            setcookie("autologin", $_SESSION['login'], time() + (86400 * 30), "/");
+        }
+
+        if(isset($_POST['submit'])) {
+            $loginClass = new Login();
+            //compare data
+            $loginClass->compare();
+        }
+
+        class Login {
+            function get_data() {
+                $_SESSION['login'] = $_POST['login'];
+                $_SESSION['password'] = $_POST['password'];
+            }
+
+            function search_user() {
+                global $con;
+
+                $sql = "SELECT * FROM users WHERE login='{$_SESSION['login']}'";
+                $query = $con->query($sql);
+
+                if($query->num_rows > 0) {
+                    $row = $query->fetch_array(MYSQLI_ASSOC);
+                    return $row;
+                }
+                else {
+                    return false;
+                }
+            }
+
+            function compare() {
+                $row = $this->search_user();
+
+                if($row != false) {
+                    $this->get_data();
+                    if($row['password'] == $_SESSION['password']) {
+                        header("Location: main.php");
+                    }
+                    else {
+                        echo "Hasło nie poprawne";
+                    }
+                }
+                else {
+                    echo "Nie ma takiego uzytkownika";
+                }
+            }
+        }
+    
+    ?>
+
+</body>
+</html>
