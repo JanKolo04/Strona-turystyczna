dc6b66628d0abf99378e634e390b38debd8d681a
JanKolo04 <jankolodziej04@gmail.com>
2022-10-25 13:03:16 +0200

    replace show design for 'Obiekty na trasie' in route page

--- trasy/trasa.php
+++ trasy/trasa.php
@@ -51,26 +51,91 @@
             ";
         }
 
-        //show objects which you can see on route
-        function show_close_objects() {
-            global $con;
+        class GetCloseObjects {
+            public $id_user;
+            public $all_objects;
+            public $all_objects_fav;
 
-            //get architect ID
-            $route_id = $_GET['trasa'];
+            function get_all_objects_from_fav($id_user) {
+                global $con;
 
-            //get all works created by this architect
-            $sql_obiekt = "SELECT * FROM Obiekty WHERE Id_trasa=$route_id";
-            $query_obiekt = mysqli_query($con, $sql_obiekt);
+                //get all objects from favorite where id_user is user id
+                $sql_fav = "SELECT Id, Id_obiektu FROM ulubione WHERE Id_uzytkownika=$id_user";
+                $query_fav = $con->query($sql_fav);
 
-            //show works
-            if($query_obiekt->num_rows > 0) {
-                while($row_works = mysqli_fetch_array($query_obiekt)) {
-                    echo "
-                        <div class='workHolder'>
-                            <img class='workImg' src='img/{$row_works['Media']}/main 1.jpeg'>
-                            <h4 class='workName'>{$row_works['Nazwa']}</h4>
-                        </div>
-                    ";
+                $array = [];
+                //if count results is bigger than 0 append data into array
+                if($query_fav->num_rows > 0) {
+                    $i=0;
+                    while($row = mysqli_fetch_array($query_fav)) {
+                        $array[$i] = [
+                            "Id"=>$row['Id'],
+                            "Id_object"=>$row['Id_obiektu']
+                        ];
+                        $i++;
+                    }
+                }
+
+                //all objects from fav
+                return $array;
+            }
+
+            function check_data_and_print() {
+                global $con;
+
+                $route_id = $_GET['trasa'];
+    
+                //get all works created by this route
+                $sql_obiekt = "SELECT Obiekty.*, Trasy.Nazwa AS 'trasa_nazwa' FROM Obiekty INNER JOIN Trasy ON Trasy.Id=Obiekty.Id_trasa WHERE Id_trasa=$route_id ORDER BY (Obiekty.Nazwa) ASC";
+                $query_obiekt = mysqli_query($con, $sql_obiekt);
+                
+                //aray with objets id from favorite where id_user is your id
+                $id_array_objects_fav = $this->get_all_objects_from_fav(3);
+
+                //print objects
+                $all_obejcts = [];
+                if($query_obiekt->num_rows > 0) {
+                    $counter=0;
+                    while($row = mysqli_fetch_array($query_obiekt)) {
+                        //variable for favorite button
+                        $fav_button_name = "favorite-button";
+                        $value = $row['Id'];
+                        $background = "out-favorite.png";
+
+                        //check which object is in favorite
+                        for($i=0; $i<sizeof($id_array_objects_fav); $i++) {
+                            if($row['Id'] == $id_array_objects_fav[$i]['Id_object']) {
+                                //update variable when object is in favorite
+                                $fav_button_name = "unfavorite-button";
+                                $value = $id_array_objects_fav[$i]['Id'];
+                                $background = "in-favorite.png";
+                                break;
+                            }
+                        }
+                        //link_to_work
+                        $link_to_work = "index.php?strona=obiekty/obiekt&obiekt={$row['Id']}&trasa={$row['Id_trasa']}";
+                        //link to img for fav_button
+                        $link_to_img_for_fav_bt = "img/icon/$background";
+                        
+                        echo "
+                            <div class='workHolder'>
+                                <a href='$link_to_work'><img class='workImg' src='img/{$row['Media']}/main 1.jpeg'></a>
+                                <div class='workInfo'>
+                                    <p class='objectLocation'><img class='iconLocation' src='img/icon/bookmark.png'> {$row['trasa_nazwa']}</p>
+                                    
+                                    <div class='workNameFavoriteHolder'>
+                                        <div class='workNameHolder'>
+                                            <a class='workName' href='$link_to_work'><h4>{$row['Nazwa']}</h4><img class='iconReadMore' src='img/icon/read-more.png'></a>
+                                        </div>
+
+                                        <div class='favoriteButtonHolder'>
+                                            <button style='background-image: url($link_to_img_for_fav_bt);' type='submit' value='$value' name='$fav_button_name' class='favorite-button'></button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        ";
+                    }
                 }
             }
         }
@@ -87,7 +152,10 @@
             <h2>Obiekty na tej trasie</h2>
         </div>
         <div id='objectsHolder'>
-            <?php show_close_objects(); ?>
+            <?php 
+                $close_obejcts = new GetCloseObjects();
+                $close_obejcts->check_data_and_print();
+            ?>
         </div>
     </div>
 
