271a4c841ae5e90161ca51b4b661701e9eb25cf9
JanKolo04 <jankolodziej04@gmail.com>
2022-10-27 10:20:38 +0200

    create better inputs in your data and create function to show password

--- konto/your-data.php
+++ konto/your-data.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <script type="text/javascript" src="js/script-account.js"></script>
 	<title>Twoje dane - Szlakiem Szczecina</title>
 </head>
 <body>
@@ -26,10 +27,21 @@
 
             function print_data($array) {
                 echo "
-                    <input type='text' name='name' value='{$array['Imie']}'>
-                    <input type='text' name='surname' value='{$array['Nazwisko']}'>
-                    <input type='email' name='email' value='{$array['Email']}'>
-                    <input type='password' name='password' value='{$array['Haslo']}'>
+                    <input class='input' type='text' name='name' value='{$array['Imie']}'>
+                    <input class='input' type='text' name='surname' value='{$array['Nazwisko']}'>
+                    <input class='input' type='email' name='email' value='{$array['Email']}'>
+                    <input class='input' id='password' type='password' name='password' value='{$array['Haslo']}'>
+
+                    <div id='other-data-holder'>
+                        <div id='show-password-holder'>
+                            <label for='show-password'>Pokaz hasło</label>
+                            <input id='show-password' type='checkbox' value='show' onclick='show_password(this)'>
+                        </div>
+
+                        <div id='submit-button-holder'>
+                            <button type='submit' name='update-user-data'>Zatwierdź</button>
+                        </div>
+                    </div>
                 ";
             }
         }
@@ -41,7 +53,7 @@
             <h3>Twoje dane</h3>
         </div>
 
-        <div id="basic-data">
+        <div id="inputs-holder">
             <?php
                 //create object
                 $user = new user_data();
