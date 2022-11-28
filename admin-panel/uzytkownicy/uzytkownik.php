79c0c61f651091dae690c8a3aa49046ac5caf3bd
JanKolo04 <jankolodziej04@gmail.com>
2022-11-28 14:19:11 +0100

    podglad uzytzkownikam

--- admin-panel/uzytkownicy/uzytkownik.php
+++ admin-panel/uzytkownicy/uzytkownik.php
@@ -3,16 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="css/style-uzytkownicy.css">
-    <script type="text/javascript" src="js/script-uzytkownicy.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/style-dodaj-uzytkownika.css">
 </head>
 <body>
 
     <?php
 
-        include("connection.php");
+        include("../connection.php");
 
-        
+        if(isset($_POST['update'])) {
+            update_data();
+        }
+
+        $user_data = user_data();      
 
         function user_data() {
             global $con;
@@ -22,67 +25,39 @@
 
             if($query->num_rows > 0 && $query != false) {
                 $row = $query->fetch_assoc();
-
-                echo "";
+                return $row;
             }
+        }
+
+        function update_data() {
+            global $con;
+            $hash = sha1($_POST['password']);
+
+            $sql = "UPDATE Uzytkownicy SET Imie='{$_POST['name']}', Nazwisko='{$_POST['surname']}', Email='{$_POST['email']}', Haslo='$hash' WHERE id_uzytkownik={$_GET['id']}";
+            $query = $con->query($sql);
         }
 
     ?>  
 
-    <div id="add-user-page">
-        <form method="POST" id="search-form">
-            <div id="search-holder">
-                <div id="search-header">
-                    <div id="input-holder" class="search-holders">
-                        <input type="text" name="search-input" id='search-input' placeholder="Wyszukaj osobę...">
-                    </div>
-                    <div id="button-search-holder" class="search-holders">
-                        <button id="search-button" name="search-button" type="submit">Szukaj</button>
-                    </div>
-                </div>
-            </form>
-        </div>
-
-        <div id='tableDiv'>
-            <div id="option-header">
-                <p>Działania masowe: </p>
-                <button onclick='delete_user()' class='delete-user-button'><i id='x-icon' class="fa-solid fa-x"></i>&nbspUsuń</button>
+    <div id="content-holder">
+        <div id="other-content-holder">
+            <div id="header">
+                <h1>Dane <?php echo $user_data['Imie']; echo ' '; echo $user_data['Nazwisko']; ?></h1>
             </div>
-
-            <div id="table-overwflow">
-                <table id="table-user">
-                    <thead>
-                        <th>
-                        <th>Id</th>
-                        <th>Imie</th>
-                        <th>Nazwisko</th>
-                        <th>Email</th>
-                        <th>Podgląd</th>
-                    </thead>
-                    <tbody>
-                        <form method="POST">
-                            <?php
-                                //variable with function which show all users
-                                $sql_all_users = "SELECT * FROM Uzytkownicy";
-
-                                //search class
-                                $search_obj = new search();
-                                if(isset($_POST['search-button'])) {
-                                    if(strlen($_POST['search-input']) > 0) {
-                                        //print whole search data
-                                        print_data($search_obj->setup_primary_data());
-                                    }
-                                    else {
-                                        print_data($sql_all_users);
-                                    }
-                                }
-                                else {
-                                    print_data($sql_all_users);
-                                }
-                            ?>
-                        </form>
-                    </tbody>
-                </table>
+            <div id="login-data-holder">
+                <form method="POST">
+                    <div id="inputs-holder">
+                        <input type="text" name="name" placeholder="Imie" class="input" value='<?php echo $user_data['Imie']; ?>'>
+                        <input type="text" name="surname" placeholder="Nazwisko" class="input" value='<?php echo $user_data['Nazwisko']; ?>'>
+                        <input type="email" name="email" placeholder="Email" class="input" value='<?php echo $user_data['Email']; ?>'>
+                        <input type="password" name="password" placeholder="Hasło" class="input" value='<?php echo $user_data['Haslo']; ?>'>
+                    </div>
+                    <div id="other-data-holder">
+                        <div id="button-holder">
+                            <button type="submit" name="update">Zatwierdź</button>
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
